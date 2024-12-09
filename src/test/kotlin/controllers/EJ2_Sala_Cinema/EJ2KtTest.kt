a3be3a613b89898e9e6512a9941ba3dc3b688c7f
Manuel Felix <manuel.felix.7e8@itb.cat>
2024-12-09 13:55:06 +0100

    EJ2 test ACABADO.

---
+++ src/test/kotlin/controllers/EJ2_Sala_Cinema/EJ2KtTest.kt
@@ -0,0 +1,21 @@
+package controllers.EJ2_Sala_Cinema
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class EJ2KtTest {
+
+@Test
+ fun manejarOpciones() {}
+}
+
+/*
+* Las funciones de este programa no devuelven ningún valor (solo realizan acciones), por lo que
+* esto hace que en Kotlin no sea posible hacer el plan de pruebas.
+*
+*Este programa se puede comprobar perfectamente ejecutándolo y ver que realmente funciona.
+* Aunque alguna función como la de inicializar la sala devuelva un valor, esta no tiene
+* ningún parámetro, con lo cual no se vería afectada en ningún caso.
+*
+*
+* */
